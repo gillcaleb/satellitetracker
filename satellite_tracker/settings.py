dfcf957f67f7c4d0813eee56b90644236093873f
Caleb Gill <gillcmc21@gmail.com>
2020-10-17 10:35:12 -0400

    setting up for Heroku

--- satellite_tracker/settings.py
+++ satellite_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -142,3 +144,5 @@ MEDIA_URL = '/media/'
 
 UPDATE_URL = 'https://celestrak.com/NORAD/elements/gp.php?GROUP=starlink&FORMAT=TLE'
 #UPDATE_URL = 'https://celestrak.com/NORAD/elements/gp.php?GROUP=GEO&FORMAT=TLE'
+
+django_heroku.settings(locals())
