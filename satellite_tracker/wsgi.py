a16687b93ee3fcadb6a521306b6a79690f46d85e
Caleb Gill <gillcmc21@gmail.com>
2020-10-23 17:56:29 -0400

    testing wsgi

--- satellite_tracker/wsgi.py
+++ satellite_tracker/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'satellite_tracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'satellitetracker.settings')
 
 application = get_wsgi_application()
