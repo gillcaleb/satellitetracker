33ff7f30706eae71b5f7a1c48cbde0fd175691d6
Caleb Gill <gillcmc21@gmail.com>
2020-10-27 08:00:19 -0400

    testing whitenoise removal

--- satellite_tracker/settings.py
+++ satellite_tracker/settings.py
@@ -156,7 +156,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
