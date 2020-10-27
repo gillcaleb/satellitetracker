020e9d62d384e052ce2f8dcb103456587bae1a65
Caleb Gill <gillcmc21@gmail.com>
2020-10-27 17:26:24 -0400

    adding media

--- satellite_tracker/settings.py
+++ satellite_tracker/settings.py
@@ -144,6 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA = '/media/'
+MEDIA_URL = os.path.join(BASE_DIR, "media")
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
